8ebe21c4dcf276573eb2f630ab1c22fedaf73378
Besker Telisma <beskertelisma9@gmail.com>
2020-12-11 08:02:17 -0600

    it

--- seeds/seed.recipes.sql
+++ seeds/seed.recipes.sql
@@ -157,12 +157,12 @@ VALUES ('Fluffy Almond Breeze Blueberry Pancakes',
 
 INSERT INTO recipes_users (user_name, full_name, password)
 VALUES
-  ('dunder', 'Dunder Mifflin', null, 'password'),
-  ('b.deboop', 'Bodeep Deboop', 'Bo', 'bo-password'),
-  ('c.bloggs', 'Charlie Bloggs', 'Charlie', 'charlie-password'),
-  ('s.smith', 'Sam Smith', 'Sam', 'sam-password'),
-  ('lexlor', 'Alex Taylor', 'Lex', 'lex-password'),
-  ('wippy', 'Ping Won In', 'Ping', 'ping-password');
+  ('dunder', 'Dunder Mifflin', 'password'),
+  ('b.deboop', 'Bodeep Deboop',  'bo-password'),
+  ('c.bloggs', 'Charlie Bloggs', 'charlie-password'),
+  ('s.smith', 'Sam Smith', 'sam-password'),
+  ('lexlor', 'Alex Taylor', 'lex-password'),
+  ('wippy', 'Ping Won In', 'ping-password');
 
 
 INSERT INTO recipes_feedbacks (
@@ -172,7 +172,7 @@ INSERT INTO recipes_feedbacks (
   recipes_id,
   user_id
 ) VALUES
-  ('great recipe as usual')
+  ('great recipe as usual', true, false);
 
 
 
